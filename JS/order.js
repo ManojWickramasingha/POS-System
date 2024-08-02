d445abd75f2f9d7e290800c90e630125e8650425
Manoj wickramasingha <141001887+ManojWickramasingha@users.noreply.github.com>
2024-08-02 09:30:02 +0530

    OrderPage responsive and add toggle menu button

--- JS/order.js
+++ JS/order.js
@@ -101,3 +101,20 @@ document.getElementById("progreFil").addEventListener("click", () => {
     table.innerHTML = content;
   });
 });
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
